663d53673c421b7fb3cb06f0d90defd4c01d410e
r1ks-iwnl <sorinesku@gmail.com>
2026-03-21 18:03:01 +0200

    modularizare pe proiecte

--- PIU_Main/Transport/Main.cs
+++ PIU_Main/Transport/Main.cs
@@ -1,12 +1,15 @@
-﻿namespace Main
+﻿using ConducatorModel = Conducator.Conducator;
+using MasinaModel = Masina.Masina;
+using CursaModel = Cursa.Cursa;
+
+namespace Main
 {
 	class Program
 	{
 		static void Main()
 		{
-			Conducator c = new Conducator("Asd Wes", "16.03.2006", "16.03.2026");
-			Console.WriteLine(c.nume + " " + c.dataNastere + " " + c.dataAngajare);
-			Console.ReadKey();
+			
 		}
 	}
 }
+
